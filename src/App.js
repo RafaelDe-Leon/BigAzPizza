983d4ee15feff8e461820c5a3e213ac4c3f1f839
Rafael De Leon <rafael_de_leon88@hotmail.com>
2020-05-14 18:04:01 -0400

    added section one

--- src/App.js
+++ src/App.js
@@ -39,63 +39,146 @@ function App() {
           </div>
         </div>
       </header>
-      <div className='wrapper mt-5'>
-        <section className='container-fluid'>
-          <div className='row'>
-            <div className='col-md-3'>
-              <div className='row'>
-                <img
-                  src='https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-1.2.1&w=1000&q=80'
-                  alt='veggie'
-                  className='img-test'
-                />
-              </div>
-              <div className='row mt-3'>
-                <h3>Lorem ipsum</h3>
-              </div>
-              <div className='row'>
-                <button className='main-button'>
-                  <span>Order Now </span>
-                </button>
-              </div>
-            </div>
 
-            <div className='col-md-3'>
-              <div className='row'>
-                <img
-                  src='https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-1.2.1&w=1000&q=80'
-                  alt='veggie'
-                  className='img-test'
-                />
-              </div>
-              <div className='row mt-3'>
-                <h3>Lorem ipsum</h3>
-              </div>
-              <div className='row'>
-                <button className='main-button'>
-                  <span>Order Now </span>
-                </button>
+      {/* Section One */}
+      {/* <div className='wrapper'> */}
+      <section className='container mt-4 mb-5'>
+        <div className='row'>
+          <div className='col-sm-12 text-center mb-4'>
+            <h2>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure
+              eius ex sunt!
+            </h2>
+          </div>
+        </div>
+        <div className='row mt-5'>
+          <div className='col-sm-3 p-4 mr-5 img-section-wrapper text-center'>
+            <div className='row'>
+              <img
+                src='https://cook.fnr.sndimg.com/content/dam/images/cook/fullset/2011/1/6/0/CCEV103_Margherita-Pizza_s4x3.jpg.rend.hgtvcom.616.462.suffix/1416867304309.jpeg'
+                alt='veggie'
+                className='section-one-img img-fluid'
+              />
+            </div>
+            <div className='row mt-1'>
+              <div className='col-sm-12'>
+                <h3 className='text-center p-1'>Margarita Pizza</h3>
               </div>
             </div>
-            <div className='col-md-3'>
-              <div className='row'>
-                <img
-                  src='https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-1.2.1&w=1000&q=80'
-                  alt='veggie'
-                  className='img-test'
-                />
-              </div>
-              <div className='row mt-3'>
-                <h3>Lorem ipsum</h3>
-              </div>
-              <div className='row'>
-                <button className='main-button'>
-                  <span>Order Now </span>
+            <div className='row mt-1 text-center'>
+              <div className='col-sm-12'>
+                <button className='main-button text-center p-1 mt-3'>
+                  <span>Order Now</span>
                 </button>
               </div>
             </div>
           </div>
-        </section>
+
+          <div className='col-sm-3 p-4 mr-5 img-section-wrapper'>
+            <div className='row'>
+              <img
+                src='https://cook.fnr.sndimg.com/content/dam/images/cook/fullset/2011/1/6/0/CCEV103_Margherita-Pizza_s4x3.jpg.rend.hgtvcom.616.462.suffix/1416867304309.jpeg'
+                alt='veggie'
+                className='section-one-img img-fluid'
+              />
+            </div>
+            <div className='row mt-1'>
+              <div className='col-sm-12'>
+                <h3 className='text-center p-1'>Margarita Pizza</h3>
+              </div>
+            </div>
+            <div className='row mt-1 text-center'>
+              <div className='col-sm-12'>
+                <button className='main-button text-center p-1 mt-3'>
+                  <span>Order Now</span>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className='col-sm-3 p-4 mr-5 img-section-wrapper'>
+            <div className='row'>
+              <img
+                src='https://cook.fnr.sndimg.com/content/dam/images/cook/fullset/2011/1/6/0/CCEV103_Margherita-Pizza_s4x3.jpg.rend.hgtvcom.616.462.suffix/1416867304309.jpeg'
+                alt='veggie'
+                className='section-one-img img-fluid'
+              />
+            </div>
+            <div className='row mt-1'>
+              <div className='col-sm-12'>
+                <h3 className='text-center p-1'>Margarita Pizza</h3>
+              </div>
+            </div>
+            <div className='row mt-1 text-center'>
+              <div className='col-sm-12'>
+                <button className='main-button text-center p-1 mt-3'>
+                  <span>Order Now</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* </div> */}
+
+      {/* Section Two */}
+
+      <div className='wrapper mt-5'>
+        <div className='container-fluid'>
+          <div className='row'>
+            <div className='col-md-12'>
+              <h2>Menu</h2>
+            </div>
+          </div>
+
+          <div className='row mb-5 mt-5'>
+            <div className='col-md-4'>
+              <img
+                src='https://images.unsplash.com/photo-1559978137-8c560d91e9e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80'
+                alt='veggie'
+                className='section-one-img'
+              />
+            </div>
+            <div className='col-md-4'>
+              <img
+                src='https://images.unsplash.com/photo-1559978137-8c560d91e9e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80'
+                alt='veggie'
+                className='section-one-img'
+              />
+            </div>
+            <div className='col-md-4'>
+              <img
+                src='https://images.unsplash.com/photo-1559978137-8c560d91e9e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80'
+                alt='veggie'
+                className='section-one-img'
+              />
+            </div>
+          </div>
+
+          <div className='row'>
+            <div className='col-md-4'>
+              <img
+                src='https://images.unsplash.com/photo-1559978137-8c560d91e9e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80'
+                alt='veggie'
+                className='section-one-img'
+              />
+            </div>
+            <div className='col-md-4'>
+              <img
+                src='https://images.unsplash.com/photo-1559978137-8c560d91e9e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80'
+                alt='veggie'
+                className='section-one-img'
+              />
+            </div>
+            <div className='col-md-4'>
+              <img
+                src='https://images.unsplash.com/photo-1559978137-8c560d91e9e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80'
+                alt='veggie'
+                className='section-one-img'
+              />
+            </div>
+          </div>
+        </div>
       </div>
       <Footer />
 
