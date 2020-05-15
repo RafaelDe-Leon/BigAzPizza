abe7673a9f6f0cf7a0466be80ba8acbcb90a22a5
Rafael De Leon <rafael_de_leon88@hotmail.com>
2020-05-14 20:13:51 -0400

    fixed route to be working now

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
-import Home from './components/Home';
+import Home from './Pages/Home';
 import MenuPG from './Pages/Menu';
 import ContactUsPG from './Pages/ContactUs';
 import LocationPG from './Pages/Location';
@@ -9,185 +9,31 @@ import LocationPG from './Pages/Location';
 import {
   BrowserRouter as Router,
   Route,
+  Switch,
+  Redirect,
   // Switch,
   // Redirect
 } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <Nav />
-      <header>
-        <div className='main-heading'>
-          <div className='row heading-padding'>
-            <div className='col-md-3'></div>
-            <div className='col-md-6 mt-5'>
-              <h1 className='heading-title'>Margarita Pizza</h1>
-              <p className='heading-p'>Taste the greatness</p>
-            </div>
-            <div className='col-md-3'></div>
-          </div>
-          <div className='row'>
-            <div className='col-md-4'></div>
-            <div className='col-md-4'></div>
-            <div className='col-md-4'>
-              <button className='main-button heading-button'>
-                <span>Order Now </span>
-              </button>
-            </div>
-          </div>
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav />
+
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/location' component={LocationPG} />
+            <Route exact path='/menu' component={MenuPG} />
+            <Route exact path='/contactus' component={ContactUsPG} />
+          </Switch>
+          <Footer />
         </div>
-      </header>
-
-      {/* Section One */}
-      {/* <div className='wrapper'> */}
-      <section className='container mt-4 mb-5'>
-        <div className='row'>
-          <div className='col-sm-12 text-center mb-4'>
-            <h2>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure
-              eius ex sunt!
-            </h2>
-          </div>
-        </div>
-        <div className='row mt-5'>
-          <div className='col-sm-3 p-4 mr-5 img-section-wrapper text-center'>
-            <div className='row'>
-              <img
-                src='https://cook.fnr.sndimg.com/content/dam/images/cook/fullset/2011/1/6/0/CCEV103_Margherita-Pizza_s4x3.jpg.rend.hgtvcom.616.462.suffix/1416867304309.jpeg'
-                alt='veggie'
-                className='section-one-img img-fluid'
-              />
-            </div>
-            <div className='row mt-1'>
-              <div className='col-sm-12'>
-                <h3 className='text-center p-1'>Margarita Pizza</h3>
-              </div>
-            </div>
-            <div className='row mt-1 text-center'>
-              <div className='col-sm-12'>
-                <button className='main-button text-center p-1 mt-3'>
-                  <span>Order Now</span>
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div className='col-sm-3 p-4 mr-5 img-section-wrapper'>
-            <div className='row'>
-              <img
-                src='https://cook.fnr.sndimg.com/content/dam/images/cook/fullset/2011/1/6/0/CCEV103_Margherita-Pizza_s4x3.jpg.rend.hgtvcom.616.462.suffix/1416867304309.jpeg'
-                alt='veggie'
-                className='section-one-img img-fluid'
-              />
-            </div>
-            <div className='row mt-1'>
-              <div className='col-sm-12'>
-                <h3 className='text-center p-1'>Margarita Pizza</h3>
-              </div>
-            </div>
-            <div className='row mt-1 text-center'>
-              <div className='col-sm-12'>
-                <button className='main-button text-center p-1 mt-3'>
-                  <span>Order Now</span>
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div className='col-sm-3 p-4 mr-5 img-section-wrapper'>
-            <div className='row'>
-              <img
-                src='https://cook.fnr.sndimg.com/content/dam/images/cook/fullset/2011/1/6/0/CCEV103_Margherita-Pizza_s4x3.jpg.rend.hgtvcom.616.462.suffix/1416867304309.jpeg'
-                alt='veggie'
-                className='section-one-img img-fluid'
-              />
-            </div>
-            <div className='row mt-1'>
-              <div className='col-sm-12'>
-                <h3 className='text-center p-1'>Margarita Pizza</h3>
-              </div>
-            </div>
-            <div className='row mt-1 text-center'>
-              <div className='col-sm-12'>
-                <button className='main-button text-center p-1 mt-3'>
-                  <span>Order Now</span>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      {/* </div> */}
-
-      {/* Section Two */}
-
-      <div className='wrapper mt-5'>
-        <div className='container-fluid'>
-          <div className='row'>
-            <div className='col-md-12'>
-              <h2>Menu</h2>
-            </div>
-          </div>
-
-          <div className='row mb-5 mt-5'>
-            <div className='col-md-4'>
-              <img
-                src='https://images.unsplash.com/photo-1559978137-8c560d91e9e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80'
-                alt='veggie'
-                className='section-one-img'
-              />
-            </div>
-            <div className='col-md-4'>
-              <img
-                src='https://images.unsplash.com/photo-1559978137-8c560d91e9e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80'
-                alt='veggie'
-                className='section-one-img'
-              />
-            </div>
-            <div className='col-md-4'>
-              <img
-                src='https://images.unsplash.com/photo-1559978137-8c560d91e9e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80'
-                alt='veggie'
-                className='section-one-img'
-              />
-            </div>
-          </div>
-
-          <div className='row'>
-            <div className='col-md-4'>
-              <img
-                src='https://images.unsplash.com/photo-1559978137-8c560d91e9e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80'
-                alt='veggie'
-                className='section-one-img'
-              />
-            </div>
-            <div className='col-md-4'>
-              <img
-                src='https://images.unsplash.com/photo-1559978137-8c560d91e9e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80'
-                alt='veggie'
-                className='section-one-img'
-              />
-            </div>
-            <div className='col-md-4'>
-              <img
-                src='https://images.unsplash.com/photo-1559978137-8c560d91e9e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80'
-                alt='veggie'
-                className='section-one-img'
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-      <Footer />
-
-      <Route exact path='/' component={Home} />
-      <Route exact path='/location' component={LocationPG} />
-      <Route exact path='/menu' component={MenuPG} />
-      <Route exact path='/contactus' component={ContactUsPG} />
-    </div>
-  );
+      </Router>
+    );
+  }
 }
 
 export default App;
