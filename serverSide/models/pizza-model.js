82ffb4279647ef9dea7c7a63e6882d28d58d6723
RafaelDe-Leon <Rafael_De_leon88@hotmail.com>
2020-05-21 15:46:20 -0400

    added readme

--- serverSide/models/pizza-model.js
+++ serverSide/models/pizza-model.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const Pizza = new Schema(
-  {
+const Pizza = new Schema({
     name: { type: String, required: true },
     time: { type: [String], required: true },
+    priceA: { type: Number, required: true },
     rating: { type: Number, required: true }
-  },
-  { timestamps: true }
-);
+}, { timestamps: true });
 
 module.exports = mongoose.model('pizzas', Pizza);
