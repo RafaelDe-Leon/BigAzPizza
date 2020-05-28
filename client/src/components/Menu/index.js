4f7a7144bccb24a249f67632af7fd36d2652e82e
sinuhe Montero <sinuhem23@gmail.com>
2020-05-28 17:17:44 -0400

    mobile working

--- client/src/components/Menu/index.js
+++ client/src/components/Menu/index.js
@@ -5,62 +5,46 @@ import { Link } from 'react-router-dom';
 export default function Menu() {
   return (
     <div>
-      <div className='container'>
-        <div className='row'>
-          <h1 className='menuMainText'>BigAzPizza's Menu</h1>
-        </div>
-        <div className='row'>
-          <p className='menuMainText2'>
-            Our menu has your favorites, our favorites, honestly, everyone's
-            favorites
-          </p>
-        </div>
-        <div className='row '>
-          <Link to='./pizzamenu' className='menuBTN '>
-            <div className='row'>
-              <div className='col-md-2 pizzaMenuIMG'></div>
-              <div className='menuItembtn col-md-10'>Pizza</div>
-            </div>
+      <div className='container centerPage'>
+        <h1 className='menuMainText'>BigAzPizza's Menu</h1>
+        <p className='menuMainText2'>
+          Our menu has your favorites, our favorites, honestly, everyone's
+          favorites
+        </p>
+        <div className='container '>
+          <Link to='./pizzamenu' className='menuBTN row'>
+            <div className='col-md-2 pizzaMenuIMG'></div>
+            <div className='menuItembtn col-md-10'>Pizza</div>
           </Link>
         </div>
-        <div className='row '>
-          <Link to='./pastamenu' className='menuBTN '>
-            <div className='row'>
-              <div className='col-md-2 pastaMenuIMG'></div>
-              <div className='menuItembtn col-md-10'>Pasta</div>
-            </div>
+        <div className='container '>
+          <Link to='./pastamenu' className='menuBTN row'>
+            <div className='col-md-2 pastaMenuIMG'></div>
+            <div className='menuItembtn col-md-10'>Pasta</div>
           </Link>
         </div>
-        <div className='row '>
-          <Link to='./saladmenu' className='menuBTN '>
-            <div className='row'>
-              <div className='col-md-2 saladMenuIMG'></div>
-              <div className='menuItembtn col-md-10'>Salad</div>
-            </div>
+        <div className='container '>
+          <Link to='./saladmenu' className='menuBTN row'>
+            <div className='col-md-2 saladMenuIMG'></div>
+            <div className='menuItembtn col-md-10'>Salad</div>
           </Link>
         </div>
-        <div className='row '>
-          <Link to='./dominicanmenu' className='menuBTN'>
-            <div className='row'>
-              <div className='col-md-2 dominicanMenuIMG'></div>
-              <div className='menuItembtn col-md-10'>Dominican</div>
-            </div>
+        <div className='container '>
+          <Link to='./dominicanmenu' className='menuBTN row'>
+            <div className='col-md-2 dominicanMenuIMG'></div>
+            <div className='menuItembtn col-md-10'>Dominican</div>
           </Link>
         </div>
-        <div className='row '>
-          <Link to='./dessertmenu' className='menuBTN'>
-            <div className='row'>
-              <div className='col-md-2 dessertMenuIMG'></div>
-              <div className='menuItembtn col-md-10'>Dessert</div>
-            </div>
+        <div className='container '>
+          <Link to='./dessertmenu' className='menuBTN row'>
+            <div className='col-md-2 dessertMenuIMG'></div>
+            <div className='menuItembtn col-md-10'>Dessert</div>
           </Link>
         </div>
-        <div className='row '>
-          <Link to='./sidesmenu' className='menuBTN'>
-            <div className='row'>
-              <div className='col-md-2 sidesMenuIMG'></div>
-              <div className='menuItembtn col-md-10'>Sides</div>
-            </div>
+        <div className='container '>
+          <Link to='./sidesmenu' className='menuBTN row'>
+            <div className='col-md-2 sidesMenuIMG'></div>
+            <div className='menuItembtn col-md-10'>Sides</div>
           </Link>
         </div>
       </div>
